e7491dc74d7f2a192f3f9e61f6711d36c568b955
arishuy <huybui19898@gmail.com>
2022-08-19 17:45:02 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import io from 'socket.io-client';
-
+import Header from './components/Header';
+import Body from './components/Body';
 const socket = io.connect('http://localhost:3001');
 
 function App() {
   return (
     <div classNameName="App">
-      {/* <Header />
+      <Header />
       <Body />
-      <Footer /> */}
-      abcde
+      {/* <Footer /> */}
     </div>
 
   );
---
+++ client/src/components/Body.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Body.css'
+
+const Body = () => {
+  return (
+    <div>
+        <div id="body">
+            <div className="sidebar">
+                abc</div>
+            </div>
+    </div>
+  )
+}
+
+export default Body
