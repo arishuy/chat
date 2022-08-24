41bf8605f931579410c88df4b74de0bb754e4a71
sumori <hoangthiphuonguyen1602@gmail.com>
2022-08-24 11:18:55 +0700

    u

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,26 +1,31 @@
 import React from 'react'
 import '../scss/components/Sidebar.css'
+import { Link } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
-        <div className="sidebar">
-                <button>
-                <i class="fa-solid fa-house"></i>
-                </button>
-                <button>
-                <i class="fa-solid fa-message"></i>
-                </button>
-                <button>
-                <i class="fa-solid fa-people-group"></i>
-                </button>
-                <button>
-                <i class="fa-solid fa-sliders"></i>
-                </button>
-                <button>
-                <i class="fa-solid fa-note-sticky"></i>
-                 </button>
-                </div>
-  )
+    <div className="sidebar">
+      <button>
+        <Link to = '/'>
+          <i class="fa-solid fa-house"></i>
+        </Link>
+      </button>
+      <button>
+        <Link to="/Message_ChatWindow">
+          <i class="fa-solid fa-message"></i>
+        </Link>
+      </button>
+      <button>
+        <i class="fa-solid fa-people-group"></i>
+      </button>
+      <button>
+        <i class="fa-solid fa-sliders"></i>
+      </button>
+      <button>
+        <i class="fa-solid fa-note-sticky"></i>
+      </button>
+    </div>
+  );
 }
 
 export default Sidebar
