dfb247d28a1416dda8ca34b10b84df262a7c45cb
arishuy <huybui19898@gmail.com>
2022-08-24 13:43:16 +0700

    update

---
+++ client/src/components/Body_contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../scss/components/Body.css'
+import Sidebar from './Sidebar'
+import Contact from './Contact'
+const Body_contact = () => {
+  return (
+    <div>
+        <div id="body">
+            <Sidebar />
+            <Contact />
+            </div>
+    </div>
+  )
+}
+
+export default Body_contact
