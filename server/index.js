c5b5f57f3c16938f48b3de71f1be9ff7f7ffd914
sumori <hoangthiphuonguyen1602@gmail.com>
2022-08-24 13:45:04 +0700

    u

--- server/index.js
+++ server/index.js
@@ -2,27 +2,19 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
-const {Server} = require('socket.io')
+const dotenv = require("dotenv");
+
+const { Server } = require('socket.io')
+const connectDB = require('./config/db')
+
 
 app.use(cors());
+app.use(express.json());
+dotenv.config();
+connectDB();
+
 
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        method: ['GET', 'POST'],
-
-    },
-}); 
-
-io.on("connection", (socket) => {
-    console.log(socket.id);
-
-    socket.on("disconnect", () => {
-        console.log("user disconnected", socket.id);
-        });
-})
-
-server.listen(3001, () => {
-    console.log('Server is running on port 3001');
+server.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 })
