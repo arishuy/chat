2c24bf6c9915ea5e9ca7cfe088695c6a79280270
arishuy <huybui19898@gmail.com>
2022-09-14 16:02:09 +0700

    update

--- client/src/components/Body/Body.js
+++ client/src/components/Body/Body.js
@@ -4,12 +4,10 @@ import Sidebar from '../Sidebar'
 import Content from '../Content'
 const Body = () => {
   return (
-    <div>
         <div id="body">
             <Sidebar />
             <Content />
             </div>
-    </div>
   )
 }
 
