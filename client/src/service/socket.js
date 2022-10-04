f45ce13667ee8efcc2d99c8a1cc93110bc5b1d12
arishuy <huybui19898@gmail.com>
2022-10-04 11:43:05 +0700

    u

--- client/src/service/socket.js
+++
@@ -1,7 +0,0 @@
-import io from "socket.io-client";
-
-export const socket = io.connect({  
-    cors: {
-    origin: "http://localhost:5000",
-    credentials: true
-  },transports : ['websocket'] });
